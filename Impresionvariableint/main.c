5e600b6538152072d76134917d9c57a382845203
Frandevep <frandevep@gmail.com>
2025-01-03 14:49:40 -0300

    Add files via upload

---
+++ Impresionvariableint/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int num=10; //dar valor numerico a una variable, num es el vector de la variable int
+
+    printf("%d",num); // imprimir esa variable con valor numerico
+
+    return 0;
+}
